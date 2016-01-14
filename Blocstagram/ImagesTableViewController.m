1a09c0ae50b7083d5e402fa54ca051aded7077d6
eric beasley <ejbeasley@gmail.com>
2016-01-13 20:34:34 -0800

    Checkpoint 26 complete

--- Blocstagram/ImagesTableViewController.m
+++ Blocstagram/ImagesTableViewController.m
@@ -14,14 +14,28 @@
 
 @implementation ImagesTableViewController
 
+- (id)initWithStyle:(UITableViewStyle)style {
+   self = [super initWithStyle:style];
+   if (self) {
+      self.images = [NSMutableArray array];
+   }
+   
+   return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+   
+   for (int i = 1; i <= 10; i++) {
+      NSString *imageName = [NSString stringWithFormat:@"%d.jpg", i];
+      UIImage *image = [UIImage imageNamed:imageName];
+      
+      if(image) {
+         [self.images addObject:image];
+      }
+   }
+   
+   [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"imageCell"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,25 +45,42 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+   
+   return self.images.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"imageCell" forIndexPath:indexPath];
     
     // Configure the cell...
-    
+   static NSInteger imageViewTag = 1234;
+   UIImageView *imageView = (UIImageView *)[cell.contentView viewWithTag:imageViewTag];
+   
+   if (!imageView) {
+      //Cell doesn't have configured image view eyt
+      imageView = [[UIImageView alloc] init];
+      imageView.contentMode = UIViewContentModeScaleToFill;
+      
+      imageView.frame = cell.contentView.bounds;
+      imageView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+      
+      imageView.tag = imageViewTag;
+      [cell.contentView addSubview:imageView];
+      
+   }
+   
+   UIImage *image = self.images[indexPath.row];
+   imageView.image = image;
+   
     return cell;
 }
-*/
+
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+   UIImage *image = self.images[indexPath.row];
+   return (CGRectGetWidth(self.view.frame) / image.size.height) * image.size.height;
+}
 
 /*
 // Override to support conditional editing of the table view.
