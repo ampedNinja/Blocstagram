eca2b7cb36cbda3a943eb5cfe193a1145daafa52
ejbeasley <ejbeasley@gmail.com>
2016-02-01 01:29:22 -0800

    Checkpoint 33 completed

---
+++ Blocstagram/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  Blocstagram
+//
+//  Created by eric on 1/30/16.
+//  Copyright © 2016 eric j beasley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+extern NSString *const LoginViewControllerDidGetAccessTokenNotification;
+
+@end
