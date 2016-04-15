660a2f70f952ea1ab88fd97a7434ba5b23462fe8
eric beasley <ericbeasley@bigbeaz.local>
2016-04-15 08:17:18 -0700

    Working copy: Checkpoint 40

---
+++ Blocstagram/ComposeCommentView.m
@@ -0,0 +1,57 @@
+//
+//  ComposeCommentView.m
+//  Blocstagram
+//
+//  Created by eric beasley on 4/14/16.
+//  Copyright © 2016 eric j beasley. All rights reserved.
+//
+
+#import "ComposeCommentView.h"
+
+@interface ComposeCommentView () <UITextViewDelegate>
+
+@property (nonatomic, strong) UITextView *textView;
+@property (nonatomic, strong) UIButton *button;
+
+@end
+
+@implementation ComposeCommentView
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        self.textView = [UITextView new];
+        self.textView.delegate = self;
+        
+        self.button = [UIButton buttonWithType:UIButtonTypeCustom];
+        [self.button setAttributedTitle:[self commentAttributedString]
+                               forState:UIControlStateNormal];
+        [self.button addTarget:self
+                        action:@selector(commentButtonPressed:)
+              forControlEvents:UIControlEventTouchUpInside];
+        
+        [self addSubview:self.textView];
+        [self.textView addSubview:self.button];
+    }
+    return self;
+}
+
+- (NSAttributedString *)commentAttributedString {
+    NSString *baseString =  NSLocalizedString(@"COMMENT", @"comment button text");
+    NSRange range = [baseString rangeOfString:baseString];
+    
+    NSMutableAttributedString *commentString = [[NSMutableAttributedString alloc] initWithString:baseString];
+    
+    [commentString addAttribute:NSFontAttributeName
+                          value:[UIFont fontWithName:@"HelveticaNeue-Bold" size:10]
+                          range:range];
+    [commentString addAttribute:NSKernAttributeName value:@1.3 range:range];
+    [commentString addAttribute:NSForegroundColorAttributeName
+                          value:[UIColor colorWithRed:0.933 green:0.933 blue:0.933 alpha:1]
+                          range:range];
+    
+    return commentString;
+}
+
+@end
